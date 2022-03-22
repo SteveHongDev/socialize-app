7adc37a23ded194b62ffc8bb968bf267a6c3ddd8
Steve Hong <ponoponong@gmail.com>
2022-03-22 21:04:25 +0900

    Initial Commit

---
+++ socialize-app/socialize_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialize_appApp.swift
+//  socialize-app
+//
+//  Created by 홍성범 on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct socialize_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
